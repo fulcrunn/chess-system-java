cfd1770d50db2c3e0387f0c8f73382be4ced2f57
AndreOliveira <fulcrunn@yahoo.com.br>
2022-01-28 16:23:31 -0300

    placing pieces on the board

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -8,10 +8,10 @@ public class ChessPiece extends Piece{
 	private Color color;
 	int moveCount;
 	
-	public ChessPiece(Board board, Color color, int moveCount) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		this.moveCount = moveCount;
+		//this.moveCount = moveCount;
 	}
 
 	public Color getColor() {
