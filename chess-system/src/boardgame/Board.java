9221015ce2dd27899b7369cfee251bd4f498977a
AndreOliveira <fulcrunn@yahoo.com.br>
2022-01-29 12:42:16 -0300

    BoardException and defensive programming

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -10,6 +10,9 @@ public class Board {
 		super();
 		this.columns = line;
 		this.rows = row;
+		if(rows<1 || columns<1) {
+			throw new BoardException("ERROR: Invalid board size, rows and columns must be major than 1.");
+		}
 		pieces = new Piece[rows][columns];
 	}
 
@@ -17,29 +20,45 @@ public class Board {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-
 	public Integer getRows() {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("ERROR: Position not exits on the board.");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("ERROR: Position not exits on the board.");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece (Piece piece, Position position) {
+		if(thereisAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-
+	
+	public boolean positionExists(int row, int column) {
+		return ((row>=0 && row < rows) && (column>=0 && column < columns));
+		//return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	public boolean thereisAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("ERROR: Position not exits on the board.");
+		}
+		return piece(position) != null;
+	}
 }
