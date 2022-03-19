3181271900d44de168f317f65fab2c0418ec335d
AndreOliveira <fulcrunn@yahoo.com.br>
2022-03-19 18:11:58 -0300

    printing possible moves

--- chess-system/src/applications/UI.java
+++ chess-system/src/applications/UI.java
@@ -56,7 +56,7 @@ public class UI { //user interface method
 		for(int i=0; i<pieces.length; i++) {
 			System.out.print((8-i) + " ");
 			for(int j=0; j<pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println("");
 		}
@@ -64,22 +64,35 @@ public class UI { //user interface method
 		System.out.println("  a b c d e f g h ");
 	}
 	
-public static void printPiece(ChessPiece piece) {
-		
-	if (piece == null) {
-        System.out.print("-");
-    }
-    else {
-        if (piece.getColor() == Color.WHITE) {
-            System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-        	//System.out.print(piece);
-        }
-        else {
-            System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-        	//System.out.print(piece);
-        }
-    }
-    System.out.print(" ");
-	}	
+	public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
+
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println("");
+		}
+
+		System.out.println("  a b c d e f g h ");
+	}
+	
+	public static void printPiece(ChessPiece piece, boolean blackGround) {
+		if(blackGround) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
+		if (piece == null) {
+			System.out.print("-" + ANSI_RESET);
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+				// System.out.print(piece);
+			} else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+				// System.out.print(piece);
+			}
+		}
+		System.out.print(" ");
+	}
 }
 
